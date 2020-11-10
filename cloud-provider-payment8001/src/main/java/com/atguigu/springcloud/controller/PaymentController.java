54a1c452360fc78d1172224c5380140f4003619d
xiajunrong <891611830@qq.com>
2020-11-10 15:31:37 +0800

    修改日志

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -17,22 +17,22 @@ public class PaymentController {
     @PostMapping(value = "/payment/create")
     public CommonResult create(Payment payment) {
         int result = paymentService.create(payment);
-        log.info("******插入结果"+result);
-        if(result>0){
-            return new CommonResult(200,"插入数据库成功",result);
-        }else{
-            return new CommonResult(444,"插入数据库失败",null);
+        log.info("******插入结果" + result);
+        if (result > 0) {
+            return new CommonResult(200, "插入数据库成功", result);
+        } else {
+            return new CommonResult(444, "插入数据库失败", null);
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
-        Payment payment  = paymentService.getPaymentById(id);
-        log.info("******插入结果"+payment);
-        if(payment != null){
-            return new CommonResult(200,"查询数据库成功",payment);
-        }else{
-            return new CommonResult(444,"没有记录"+id,null);
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("******查询结果" + payment);
+        if (payment != null) {
+            return new CommonResult(200, "查询数据库成功", payment);
+        } else {
+            return new CommonResult(444, "没有记录" + id, null);
         }
     }
 }
